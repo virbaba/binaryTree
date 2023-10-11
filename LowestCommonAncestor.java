2f2925e63903be2f63ccfff6551d7b82acde6d71
virbaba <109612471+virbaba@users.noreply.github.com>
2023-10-11 14:26:11 +0530

    Create LowestCommonAncestor.java

---
+++ LowestCommonAncestor.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+/************************************************************
+
+ Following is the TreeNode class structure
+
+ class TreeNode<T>
+ {
+     T data;
+     TreeNode<T> left;
+     TreeNode<T> right;
+
+     TreeNode(T data)
+     {
+         this.data = data;
+         left = null;
+         right = null;
+     }
+ };
+
+ ************************************************************/
+
+public class Solution {
+    
+    public static int lowestCommonAncestor(TreeNode<Integer> root, int x, int y) {
+        //    Write your code here.
+        if(root == null)
+            return -1;
+
+        if(root.data == x || root.data == y)
+            return root.data;
+
+        int left = lowestCommonAncestor(root.left, x, y);
+        int right = lowestCommonAncestor(root.right, x, y);
+
+        
+        if(left != -1 && right != -1)
+            return root.data;
+
+        if(left != -1)
+            return left;
+
+        return right;
+    }
+}
